72aadc1307498820272d45590923fa81f2827da6
川口拓巳 <kawaguchitakumi@kawaguchitakuminoMacBook-Pro.local>
2018-01-30 09:22:57 +0900

    第3章終了

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :fan_comments
+  resources :reviews
+  resources :authors
+  resources :users
+  resources :books
   get 'hello/list'
 end
